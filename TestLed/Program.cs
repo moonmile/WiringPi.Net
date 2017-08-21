c59bb1e7dbd46c171397b5325e2b13b2a7d13aa9
Tomoaki Masuda <masuda@moonmile.net>
2017-08-22 03:03:04 +0900

    Wiring Pi を .NET Core 化して TestLED を追加

---
+++ TestLed/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using WiringPi;
+using System.Threading.Tasks;
+namespace TestLed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LED on Wiring Pi.");
+            var app = new Program();
+            app.Go(args);
+            Console.ReadKey();
+        }
+
+        public async void Go( string[] args )
+        {
+            int ret = Init.WiringPiSetupGpio();
+            if (ret == -1)
+            {
+                Console.WriteLine("Init failed: {0}", ret);
+                return;
+            }
+
+            GPIO.pinMode(27, (int)GPIO.GPIOpinmode.Output);
+            for (int i = 0; i < 5; i++)
+            {
+                GPIO.digitalWrite(27, 1);
+                await Task.Delay(500);
+                GPIO.digitalWrite(27, 0);
+                await Task.Delay(500);
+            }
+
+        }
+    }
+}
